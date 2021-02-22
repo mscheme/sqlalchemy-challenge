0c5bc85fb6c0ceae78e50c2c12b50a2bf4da4813
mscheme <maraschemel@gmail.com>
2021-02-22 04:56:24 -0500

    modified sessions

--- app.py
+++ app.py
@@ -25,9 +25,6 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-# Create our session (link) from Python to the DB
-session = Session(engine)
-
 #################################################
 # Flask Setup
 #################################################
@@ -56,6 +53,10 @@ def precipitation():
     # Convert the query results to a dictionary using date as the key and prcp as the value.
     # Return the JSON representation of your dictionary.
     precip={}
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
     # find the latest date in the data set
     max_date = session.query(func.max(Measurement.date)).first()[0]
 
@@ -64,6 +65,9 @@ def precipitation():
 
     query_results = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= prev_year_date).all()
+
+    # close session
+    session.close()
 
     for row in query_results:
         date = (row[0])
@@ -75,8 +79,14 @@ def precipitation():
 
 @app.route("/api/v1.0/stations")
 def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
     #Return a JSON list of stations from the dataset
     station_results = session.query(Station.station).all()
+
+    #close session
+    session.close()
 
     # Convert list of tuples into normal list
     station_list = list(np.ravel(station_results))
@@ -86,6 +96,9 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def tobs():
     
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
     max_date = session.query(func.max(Measurement.date)).first()[0]
 
     # subtract one year from the max date
@@ -104,6 +117,9 @@ def tobs():
         filter(Measurement.station == most_active_station).\
             filter(Measurement.date >= prev_year_date).all()
 
+    # close session
+    session.close
+
      # Convert list of tuples into normal list
     tobs_list = list(np.ravel(active_station_obs))
 
@@ -119,6 +135,9 @@ def minAvgMax(start=None, end=None):
     # define select statement
     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
     #When given the start only, calculate TMIN, TAVG, and TMAX 
     # for all dates greater than and equal to the start date.
     if not end:
@@ -133,6 +152,9 @@ def minAvgMax(start=None, end=None):
     # for dates between the start and end date inclusive.
     st_end_results = session.query(*sel).filter(Measurement.date >= start).\
         filter(Measurement.date <= end).all()
+
+    # close session
+    session.close()
 
     # Convert list of tuples into normal list
     st_end_list = list(np.ravel(st_end_results))
