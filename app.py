f003cd67409897f1f8bc9b447fc6532740372f75
mscheme <maraschemel@gmail.com>
2021-02-22 04:25:15 -0500

    station and tobs route

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ def home():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    # Return the precipitation data for the last year
     # Convert the query results to a dictionary using date as the key and prcp as the value.
     # Return the JSON representation of your dictionary.
     precip={}
@@ -71,6 +72,43 @@ def precipitation():
         precip[date] = prcp
     
     return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    #Return a JSON list of stations from the dataset
+    station_results = session.query(Station.station).all()
+
+    # Convert list of tuples into normal list
+    station_list = list(np.ravel(station_results))
+
+    return jsonify(station_list = station_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    
+    max_date = session.query(func.max(Measurement.date)).first()[0]
+
+    # subtract one year from the max date
+    prev_year_date = dt.datetime.strptime(max_date, '%Y-%m-%d') - relativedelta(years=1)
+
+    # identify the most active station
+    active_stations = session.query(Measurement.station, func.count(Measurement.station)).\
+    group_by(Measurement.station).\
+    order_by(func.count(Measurement.station).desc()).\
+    all()
+
+    most_active_station = active_stations[0][0]
+
+    # query the tobs for the station over the last yera of data
+    active_station_obs = session.query(Measurement.tobs).\
+        filter(Measurement.station == most_active_station).\
+            filter(Measurement.date >= prev_year_date).all()
+
+     # Convert list of tuples into normal list
+    tobs_list = list(np.ravel(active_station_obs))
+
+    return jsonify(Temperature_Observations = tobs_list)
+
 
 if __name__ == '__main__':
     app.run()
